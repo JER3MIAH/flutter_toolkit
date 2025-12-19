3f5255fcf85e3e5113d93076618a05b2eae2c3fa
JER3MIAH <jeremiahbrownie@gmail.com>
2025-12-19 18:34:05 +0100

    refactor: rename AppTheme class to BlueTheme for consistency in theme naming

--- lib/src/themes/app_theme.dart
+++ lib/src/themes/app_theme.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_toolkit/src/themes/colors.dart';
 
-class AppTheme {
+class BlueTheme {
   static ThemeData light = ThemeData(
     useMaterial3: true,
     scaffoldBackgroundColor: AppColors.neutral100,
